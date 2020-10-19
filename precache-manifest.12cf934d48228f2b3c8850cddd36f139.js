1fe5db3468b8d3d35ab5a592d054776a67005415
Your Name <you@example.com>
2020-10-19 12:45:00 +0530

    Updates

---
+++ precache-manifest.12cf934d48228f2b3c8850cddd36f139.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8c72ce51a8c532e84b88dccf38d99af",
+    "url": "/das/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/das/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "953550e9b1e852594690",
+    "url": "/das/static/css/main.c26483de.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/das/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/das/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "953550e9b1e852594690",
+    "url": "/das/static/js/main.f79137a0.chunk.js"
+  },
+  {
+    "revision": "02a3f7879fc89380e4d7",
+    "url": "/das/static/js/runtime-main.de8f14d5.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/das/static/media/SEGA.12a3db8b.woff"
+  }
+]);
